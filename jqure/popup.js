7b51a1cf5407171fb823e7575a79e798ec3ded7e
guswjdl <guswjddudwo@naver.com>
2017-12-19 17:15:09 +0900

    팝업

--- jqure/popup.js
+++ jqure/popup.js
@@ -1,31 +1,29 @@
 $(function() {
+    
+    
   
     //글을 누르면 함수 실행
-    $('#popup').after().click(function() {
+    $('.popup a').click(function(e) {
+        
+        e.preventDefault();
         
         //bgArea를 보여줌 (실제로 보이진 않음 투명도가0이라서)
         $('#befor_box').show();
+        $('#befor_box').animate( { 'opacity':1 },1000, "easeOutExpo" ); 
         
-        //bgArea를 서서히 나타냄
+    });  
+
+        
+        $('#befor_box a').click(function(e) {
+        
+        e.preventDefault();
+        $('#befor_box').hide();       
+       
         $('#befor_box').animate( { 'opacity':1 },1000, "easeOutExpo" );
-        
-        //popup영역을 서서히 원본비율로 키움
-        $('.popup').transition( { 
-            'translate':'-50%, -50%' ,
-            'scale':1            
-        },1000, "easeOutExpo" );
-        
-        //#instaArea크기를 70%로 줄이는 애니매이션 
-//        $('#instaArea').transition( {  
-//            'translate' : '-50%, -50%',
-//            'scale': 0.7
-//        },1000, "easeOutExpo" );
-        
-        //body 선의 두께를 50px로 늘리는 애니매이션 
-//        $('body').animate( { 'border-width' : 50 },1000, "easeOutExpo" );
-        
-        //클릭한것의 이전요소가 가지는 텍스트값을 가져옴 
-        $('.popup').text( $(this).find('p').text() );
+       
+
     });
+    
+     
 });
 
