51e19df5cdfaaf2bc0285f05087acdf8fbd83991
guswjdl <34507024+guswjdl@users.noreply.github.com>
2017-12-19 15:50:00 +0900

    팝업완료

---
+++ popup.js
@@ -0,0 +1,29 @@
+$(function() {
+    
+    
+  
+    //글을 누르면 함수 실행
+    $('.popup a').click(function(e) {
+        
+        e.preventDefault();
+        
+        //bgArea를 보여줌 (실제로 보이진 않음 투명도가0이라서)
+        $('#befor_box').show();
+         
+        
+    });  
+
+        
+        $('#befor_box a').click(function(e) {
+        
+        e.preventDefault();
+        $('#befor_box').hide();       
+       
+        $('#befor_box').animate( { 'opacity':0 },1000, "easeOutExpo" );
+       
+
+    });
+    
+     
+});
+
