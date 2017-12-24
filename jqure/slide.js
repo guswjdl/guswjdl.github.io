b7c9cbf5df1d0620e5041b21d45c9982ee021868
guswjdl <guswjddudwo@naver.com>
2017-12-24 18:44:28 +0900

    slide

--- jqure/slide.js
+++ jqure/slide.js
@@ -4,15 +4,15 @@ $('.slide_btn').click(function(e){
     var target = list.find('li').eq(0);
     
     //1번방법
-//    list.find('ul').append(target);
+    list.find('ul').append(target);
     
     //2번방법
-        var height = target.height();
-        list.animate({
-            top: '-'+height
-        }, 300, function(){
-            $(this).find('ul').append(target);
-            $(this).removeAttr('style');;
+//        var height = target.height();
+//        list.animate({
+//            top: '-'+height
+//        }, 300, function(){
+//            $(this).find('ul').append(target);
+//            $(this).removeAttr('style');
         });
     
 
